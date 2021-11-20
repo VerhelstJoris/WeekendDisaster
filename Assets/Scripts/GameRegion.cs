96a83c19dc23ae85cbdde5c20a14f7a123068db3
Erik Molenaar <enmolenaar@gmail.com>
2021-11-20 02:12:26 +0000

    Upgraded project to Universal Render Pipeline, gave colour to the world

---
+++ Assets/Scripts/GameRegion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameRegion : MonoBehaviour
+{
+    [SerializeField]
+    private Regions region;
+    private float happiness = 1.0f, money = 1.0f, energy = 1.0f;
+
+    public float Happiness
+    {
+        get { return happiness; }
+    }
+
+    public float Money {
+        get { return money; }
+    }
+
+    public float Energy
+    {
+        get { return energy; }
+    }
+
+    public void Select()
+    {
+        Debug.Log("Selected: " + region.ToString());
+    }
+
+    public void Deselect()
+    {
+
+    }
+}
---
+++ Assets/Scripts/InputSelectionHelper.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class InputSelectionHelper : MonoBehaviour
+{
+    private GameRegion selectedRegion;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            HandleTouch(Input.mousePosition, TouchPhase.Ended);
+        }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            HandleTouch(touch.position, touch.phase);
+        }
+    }
+
+    private void HandleTouch(Vector2 TouchPos, TouchPhase TouchPhase)
+    {
+        if (TouchPhase == TouchPhase.Ended)
+        {
+            Ray ray = cam.ScreenPointToRay(TouchPos);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                GameRegion hitRegion = hitInfo.collider.GetComponent<GameRegion>();
+                if (hitRegion != null)
+                {
+                    SelectRegion(hitRegion);
+                }
+            }
+        }
+    }
+
+    private void SelectRegion(GameRegion newRegion)
+    {
+        if (selectedRegion != null)
+        {
+            selectedRegion.Deselect();
+        }
+        selectedRegion = newRegion;
+        newRegion.Select();
+    }
+}
